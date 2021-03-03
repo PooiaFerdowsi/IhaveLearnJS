e02f9455e3e88a802c6cac2995b75488e4fbebb7
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-03-03 19:51:49 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 var x = undefined;
 var y;
+var z = x === y ? true : false;
+let me = {
+name:"Pooia",
+lname:"Ferdowsi",
+age:"12",
+langs:"PHP,Py,Java,C,JS,CSS,HTML",
+frameworks:"Flask , Laravel",
+os:"win7",
+wishes:"http server , os , programming languages"
+};
+if (me.name != "Pooia"){
+    windiw.print()
+} else {
+  let c = confirm("name of me is Pooia")
+  c ? document.write("Thanks") : document.write("for what");
+}
+const PI = 3.1415;
+// define function
+function basicFunc(args){
+  return "Hello World!"
+}
+
+jsFunc = (args) => "</h1>Template</h1>"
+
+jsFunc0 = () => {
+  /// codes
+  let xyz = "<h1> Title Is ? </h1>";
+  return xyz;
+  
+  
